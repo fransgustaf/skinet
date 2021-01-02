8bd761c5166554c4dd804d49dcb646fa8153ab6e
Gustaf Byström <gustaf@formidableservice.com>
2021-01-02 15:21:46 +0100

    End of section 5 Error Handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundResult()
+        {
+            var product = _context.Products.Find(999);
+
+            if(product == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var product = _context.Products.Find(999);
+
+            var productToReturn = product.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundResult(int id)
+        {
+            return Ok();
+        }
+    }
+}
