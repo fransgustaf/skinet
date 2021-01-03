a163e12430cdb29eef047eee506ded863fb210fa
Gustaf Byström <gustaf@formidableservice.com>
2021-01-03 11:58:36 +0100

    Section 6 API Paging, Filtering, Sorting and Searching

--- Core/entities/IGenericRepository.cs
+++ Core/entities/IGenericRepository.cs
@@ -10,5 +10,6 @@ namespace Core.Entities
         Task<IReadOnlyList<T>> ListAllAsync();
         Task<T> GetEntityWithSpec(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<int> CountAsync(ISpecification<T> spec);
     }
 }
