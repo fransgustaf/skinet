1f4079f75d8ba40188cab822ab9050e2f0f19f4f
Gustaf Byström <gustaf@formidableservice.com>
2021-01-06 13:10:45 +0100

    End of Section 13 Install and setup Redis

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id) {
+            var basket = await _basketRepository.GetBasketASync(id);
+
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket customerBasket) {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
+
+            return Ok(updatedBasket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasketAsync(string id) {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
